5938cc3d48f3163547fc46047149d06384b8b8c5
spongah <rocksolid801@gmail.com>
2017-10-08 10:13:28 -0700

    change root path to junior developer

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.render('pages/junior_developer');
 });
 
 app.get('/junior_developer', function(request, response) {
