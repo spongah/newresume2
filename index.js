f617c9f88a6ece82476c054b32a1bfda6cc4e613
spongah <rocksolid801@gmail.com>
2017-10-08 08:51:31 -0700

    add /junior_developer initial

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/junior_developer', function(request, response) {
+  response.render('pages/junior_developer');
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
